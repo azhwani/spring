b0578f3bb9166617254df990467cb2b0ccc59de3
Azhwani <azh.abderrahim@gmail.com>
2019-09-18 11:47:15 +0100

    Create SpringUtil.java

---
+++ SpringUtil.java
@@ -0,0 +1,24 @@
+package java.spring.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.spring.ioc.EmployeDAO;
+
+public class SpringUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		@SuppressWarnings({ "resource", "unused" })
+		ApplicationContext app1 = new AnnotationConfigApplicationContext(Springloc.class);
+		@SuppressWarnings({ "resource" })
+		ApplicationContext app2 = new ClassPathXmlApplicationContext("META-INF/ApplicationContext.xml");
+		
+		EmployeDAO emp = app2.getBean(EmployeDAO.class);
+		System.out.println(emp.printfirstname());
+
+	}
+
+}
