fc6ba9eea2c285e3c277ac8f7372b2ce566a245a
Azhwani <azh.abderrahim@gmail.com>
2019-09-18 11:48:02 +0100

    Update SpringUtil.java

--- SpringUtil.java
+++ SpringUtil.java
@@ -1,5 +1,3 @@
-package java.spring.ioc;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
