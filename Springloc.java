3299d47ae9dee529d9f127df1c69b415477ab6b8
Azhwani <azh.abderrahim@gmail.com>
2019-09-18 11:42:56 +0100

    Create Springloc.java

---
+++ Springloc.java
@@ -0,0 +1,20 @@
+package java.spring.ioc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.spring.ioc.Employe;
+
+@Configuration
+@ComponentScan("java.spring.ioc")
+public class Springloc {
+	
+	  @Bean
+    public Employe getEmploye() {
+          Employe empl = new Employe();
+          empl.setFirstname("Diana");
+          return empl;
+    }
+
+}
